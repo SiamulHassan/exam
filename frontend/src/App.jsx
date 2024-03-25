f38c4890efbdd9937169b13c1c97c9e03c941307
SiamulHassan <syeam45@gmail.com>
2024-03-25 15:23:43 +0600

    finall 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <p>app</p>
-    </>
-  );
-}
+const App = () => {
+  return <div>skldjfskljsdf</div>;
+};
 
 export default App;
